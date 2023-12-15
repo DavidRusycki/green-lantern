9d65c92b73fec6b4f88535e085a445a2d9e58024
david.rusycki <davidrusyck@gmail.com>
2023-12-14 22:01:12 -0300

    feat: add initial struture of project

---
+++ src/main/java/com/dev/greenlantern/GreenLanternApplication.java
@@ -0,0 +1,13 @@
+package com.dev.greenlantern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenLanternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenLanternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/greenlantern/GreenLanternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.greenlantern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenLanternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
