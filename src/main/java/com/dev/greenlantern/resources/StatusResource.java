e34e14a0fa80de94064b6dfb05c4d739057d8531
david.rusycki <davidrusyck@gmail.com>
2023-12-14 22:30:06 -0300

    feat: add org.json dependency

--- src/main/java/com/dev/greenlantern/resources/StatusResource.java
+++ src/main/java/com/dev/greenlantern/resources/StatusResource.java
@@ -1,17 +1,26 @@
 package com.dev.greenlantern.resources;
 
+import jdk.jfr.ContentType;
 import org.apache.tomcat.util.json.JSONParser;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("/api/health")
 public class StatusResource {
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public String check() {
-        return "Application is up and running";
+        JSONObject response = new JSONObject();
+        response.put("status", "running");
+        response.put("time", System.currentTimeMillis());
+
+        return response.toString();
     }
 
 }
