ec2c2a1edd46addbed4d9b5c50b946204c53ad91
david.rusycki <davidrusyck@gmail.com>
2023-12-16 17:30:19 -0300

    feat: add QR generation

---
+++ src/main/java/com/dev/greenlantern/domain/entity/QrEntity.java
@@ -0,0 +1,8 @@
+package com.dev.greenlantern.domain.entity;
+
+import lombok.Data;
+
+@Data
+public class QrEntity {
+    private String content;
+}
--- src/main/java/com/dev/greenlantern/resources/QrcodeResource.java
+++ src/main/java/com/dev/greenlantern/resources/QrcodeResource.java
@@ -1,12 +1,23 @@
 package com.dev.greenlantern.resources;
 
+import com.dev.greenlantern.domain.entity.QrEntity;
+import net.glxn.qrgen.javase.QRCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 @RestController
 @RequestMapping("/api/v1/qrcode")
 public class QrcodeResource {
 
+    @GetMapping("/new")
+    public String generate(@RequestBody QrEntity qrEntity) {
+        File file = QRCode.from(qrEntity.getContent()).file();
 
+        return file.getAbsolutePath();
+    }
 
 }
